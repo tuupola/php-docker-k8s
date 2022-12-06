31ba06e5f91bcce58850f1bc9793d3ff169b9e44
Mika Tuupola <tuupola@appelsiini.net>
2022-12-06 21:33:17 +0700

    Add Apache + PHP example

---
+++ app.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . "/vendor/autoload.php";
+
+$app = AppFactory::create();
+
+$app->addRoutingMiddleware();
+$app->addBodyParsingMiddleware();
+$app->addErrorMiddleware(true, true, true);
+
+$app->get("/[{name}]", function (
+    ServerRequestInterface $request,
+    ResponseInterface $response,
+    array $args
+): ResponseInterface {
+    $name = $args["name"] ?? "world";
+    $response->getBody()->write("hello $name");
+    return $response;
+});
+
+$app->run();
