50add03a3a032299568e76788272ba855ef0a41f
Mika Tuupola <tuupola@appelsiini.net>
2022-12-07 22:04:34 +0700

    Add endpoint for fetching cars from database

--- app.php
+++ app.php
@@ -23,6 +23,23 @@ $app->get("/server", function (
     return $response;
 });
 
+$app->get("/cars", function (
+    ServerRequestInterface $request,
+    ResponseInterface $response,
+    array $arguments
+): ResponseInterface {
+
+    $pdo = new PDO("mysql:host=mariadb;dbname=example", "user", "password");
+
+    $statement = $pdo->prepare("SELECT brand FROM cars");
+    $statement->execute();
+    $result = $statement->fetchAll(PDO::FETCH_OBJ);
+    foreach ($result as $row) {
+        $response->getBody()->write("{$row->brand} ");
+    }
+    return $response;
+});
+
 $app->get("/[{name}]", function (
     ServerRequestInterface $request,
     ResponseInterface $response,
